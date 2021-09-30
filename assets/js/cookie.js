63f91429f61c8712e3596a4363151287fd0ea2fe
oxmc <67136658+oxmc@users.noreply.github.com>
2021-09-30 16:25:14 -0700

    Create cookie.js

---
+++ assets/js/cookie.js
@@ -0,0 +1,11 @@
+/* Set variables */
+var clicks = 0;
+
+/* Check if cookies have been clicked before */
+
+
+/* Functions */
+function addCookie(amount) {
+  clicks = clicks + amount;
+  counter.innerHTML = clicks;
+}
